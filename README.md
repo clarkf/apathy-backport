79b5c03a2a55ce40fd5a504c2db04ff476e296ea
Clark Fischer <clark.fischer@gmail.com>
2023-10-13 14:46:03 -0700

    Remove configuration
    For the time being, there's no good reason for configuration, which
    means the code could be simplified pretty significantly.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ installed, hostile mobs will be unable to target or harm players. This
 allows players to engage in combat selectively, while still having
 access to mob drops.
 
+Flexibility and configuration is a non-goal for this mod. If you need
+to fight a mob that will target you back (e.g. Ender Dragon, Twilight
+Forest's Lich), disable the mod temporarily.
+
 ## Why?
 
 Many players feel that Minecraft's combat mechanics are dull, or even
@@ -28,6 +32,18 @@ This mod brings the same benefits to 1.7.10 modpacks.
 * [quat1024's Apathy Mod](https://github.com/quat1024/apathy) for
   various combinations of Forge and Fabric for Minecraft 1.16, 1.18,
   1.19
+
+## Contributing
+This project is based off GT New Horizon's [example
+mod][gtnh-example-mod]. Use the documentation there to get your
+development environment up and running. tl;dr: run `./gradlew build`
+once to set up the environment, then open in your editor/IDE of
+choice.
+
+[gtnh-example-mod]: https://github.com/GTNewHorizons/ExampleMod1.7.10
+
+Patches are accepted via email or [Github's pull request
+system](https://github.com/clarkf/apathy-backport/pulls).
 
 ## License
 MIT License. See LICENSE for the full text.
--- src/main/java/net/clarkf/apathy/ApathyMod.java
+++ src/main/java/net/clarkf/apathy/ApathyMod.java
@@ -1,24 +1,64 @@
 package net.clarkf.apathy;
 
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
 public class ApathyMod {
 
     public static final Logger LOG = LogManager.getLogger(Tags.MODID);
 
-    @SidedProxy(clientSide = "net.clarkf.apathy.ClientProxy", serverSide = "net.clarkf.apathy.CommonProxy")
-    public static CommonProxy proxy;
-
     @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(event);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    /**
+     * Cancels targeting of players by mobs.
+     */
+    @SubscribeEvent
+    public void apathy(LivingSetAttackTargetEvent event) {
+        if (event.entityLiving.worldObj.isRemote) return;
+
+        EntityLivingBase base = event.entityLiving;
+        // When a player attacks an enemy, they set a special "revenge" target to that player. For whatever
+        // reason, the getter is called "AITarget".
+        if (base.getAITarget() instanceof EntityPlayer) {
+            base.setRevengeTarget(null);
+        }
+
+        // When an enemy sees a player, they become the 'attack target'. If the aggressor has an attack target,
+        // null it out.
+        if (base instanceof EntityLiving living) {
+            if (living.getAttackTarget() instanceof EntityPlayer) {
+                living.setAttackTarget(null);
+            }
+        }
+    }
+
+    /**
+     * A handler which cancels damage to players. While {@link #apathy(LivingSetAttackTargetEvent)} prevents mobs from
+     * targeting players, mobs like slimes move randomly and cause damage if they intersect with anything.
+     */
+    @SubscribeEvent
+    public void ignoreDamage(LivingHurtEvent event) {
+        if (event.entityLiving.worldObj.isRemote) return;
+        if (!(event.entityLiving instanceof EntityPlayer)) return;
+
+        if (event.source.getDamageType()
+            .equals("mob")) {
+            event.setCanceled(true);
+        }
     }
 }
